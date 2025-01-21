b73ff16cafd1b0660c8a0a1eb959785337a2729c
Peter Bauer <peter.bauer@bajupa.com>
2025-01-21 09:30:42 +0100

    Moved JavaScript to external file

---
+++ Units/REST/public/js/main.js
@@ -0,0 +1,13 @@
+document.body.onload = function() {
+
+}
+
+// Script to open and close sidebar
+function showSidebar() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+   
+  function hideSidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+  
