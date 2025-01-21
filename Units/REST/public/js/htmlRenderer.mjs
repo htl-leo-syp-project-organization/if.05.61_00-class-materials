c8b2d8ca4a9eda990ca5fbba7cbfeffa8610c01e
Peter Bauer <peter.bauer@bajupa.com>
2025-01-21 17:34:27 +0100

    Somehow works but is shit

--- Units/REST/public/js/htmlRenderer.mjs
+++ Units/REST/public/js/htmlRenderer.mjs
@@ -1,9 +1,13 @@
 export function renderOneGridLine(blogEntries) {
-    const grid = document.querySelector('#food')
+    const gridLine = document.createElement('div')
+    gridLine.classList.add('w3-row-padding')
+    gridLine.classList.add('w3-padding-16')
+    gridLine.classList.add('w3-center')
     blogEntries.forEach(blogEntry => {
       const oneGridEntry = renderOneGrid(blogEntry)
-      grid.appendChild(oneGridEntry)
+      gridLine.appendChild(oneGridEntry)
     })
+    return gridLine
   }
   
   function renderOneGrid(blogEntry) {
@@ -28,3 +32,30 @@ export function renderOneGridLine(blogEntries) {
     return newDiv
   }
   
+
+//   <!-- First Photo Grid-->
+//   <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+//   </div>
+  
+//   <!-- Second Photo Grid-->
+//   <div class="w3-row-padding w3-padding-16 w3-center">
+//     <div class="w3-quarter">
+//       <img src="/images/popsicle.jpg" alt="Popsicle" style="width:100%">
+//       <h3>All I Need Is a Popsicle</h3>
+//       <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+//     </div>
+//     <div class="w3-quarter">
+//       <img src="/images/salmon.jpg" alt="Salmon" style="width:100%">
+//       <h3>Salmon For Your Skin</h3>
+//       <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+//     </div>
+//     <div class="w3-quarter">
+//       <img src="/images/sandwich.jpg" alt="Sandwich" style="width:100%">
+//       <h3>The Perfect Sandwich, A Real Classic</h3>
+//       <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+//     </div>
+//     <div class="w3-quarter">
+//       <img src="/images/croissant.jpg" alt="Croissant" style="width:100%">
+//       <h3>Le French</h3>
+//       <p>Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+//     </div>
--- Units/REST/public/js/main.mjs
+++ Units/REST/public/js/main.mjs
@@ -2,20 +2,18 @@ import { getData } from './rest.mjs'
 import { renderOneGridLine } from './htmlRenderer.mjs'
 
 document.body.onload = function() {
+  // const sideBar = document.querySelector('#mySidebar')
+  // sideBar.addEventListener('click', hideSidebar)
     loadDataFromServer()
 }
 
 async function loadDataFromServer() {
     const blogEntries = await getData("http://localhost:3000/posts")
-    renderOneGridLine(blogEntries.slice(0, 4))
+    const grid = document.querySelector('#food-grid')
+    for (let i = 0; i < blogEntries.length; i+=4) {
+        const oneGridLine = renderOneGridLine(blogEntries.slice(i, i+4))
+        grid.appendChild(oneGridLine)
+    }
+    // renderOneGridLine(blogEntries.slice(0, 4))
     console.log(blogEntries)
-}
-
-// Script to open and close sidebar
-function showSidebar() {
-    document.getElementById("mySidebar").style.display = "block"
-}
-   
-function hideSidebar() {
-  document.getElementById("mySidebar").style.display = "none"
 }
