37e8efd1a3a79ad54ed55717169c7640366d354c
Peter Bauer <peter.bauer@bajupa.com>
2025-01-21 14:55:06 +0100

    Render one element dynamically

--- Units/REST/public/js/main.mjs
+++ Units/REST/public/js/main.mjs
@@ -1,18 +1,54 @@
-import { getData } from './rest.mjs';
+import { getData } from './rest.mjs'
+
 document.body.onload = function() {
     loadDataFromServer()
 }
 
 async function loadDataFromServer() {
-    const blogEntries = await getData("http://localhost:3000/posts");
-    console.log(blogEntries);
+    const blogEntries = await getData("http://localhost:3000/posts")
+    renderOneGridLine(blogEntries.slice(0, 3))
+    console.log(blogEntries)
+}
+
+function renderOneGridLine(blogEntries) {
+  const grid = document.querySelector('#food')
+  const oneGridEntry = renderOneGrid(blogEntries[0])
+  grid.appendChild(oneGridEntry)
+}
+
+function renderOneGrid(blogEntry) {
+  const newDiv = document.createElement('div')
+  newDiv.classList.add('w3-quarter')
+
+  const newImg = document.createElement('img')
+  newImg.src = blogEntry.imagePath
+  newImg.alt = 'food image'
+  newImg.style.width = '100%'
+
+  const newH3 = document.createElement('h3')
+  newH3.textContent = blogEntry.title
+
+  const newP = document.createElement('p')
+  newP.textContent = blogEntry.text
+
+  newDiv.appendChild(newImg)
+  newDiv.appendChild(newH3)
+  newDiv.appendChild(newP)
+
+  return newDiv
+  //   return `
+  //     <div class="w3-quarter">
+  //       <img src=${blogEntry.imagePath} alt="food image" style="width:100%">
+  //       <h3>${blogEntry.title}</h3>
+  //       <p>${blogEntry.text}</p>
+  //     </div>
 }
 
 // Script to open and close sidebar
 function showSidebar() {
-    document.getElementById("mySidebar").style.display = "block";
-  }
+    document.getElementById("mySidebar").style.display = "block"
+}
    
-  function hideSidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+function hideSidebar() {
+  document.getElementById("mySidebar").style.display = "none"
+}
