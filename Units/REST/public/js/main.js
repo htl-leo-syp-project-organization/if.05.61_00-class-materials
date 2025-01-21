0ae18b197885b7250f2b71e911a78dc240ed2b17
Peter Bauer <peter.bauer@bajupa.com>
2025-01-21 10:37:08 +0100

    Added first get request

--- Units/REST/public/js/main.js
+++ Units/REST/public/js/main.js
@@ -1,5 +1,5 @@
 document.body.onload = function() {
-
+    getData("http://localhost:3000/posts");
 }
 
 // Script to open and close sidebar
@@ -11,3 +11,16 @@ function showSidebar() {
     document.getElementById("mySidebar").style.display = "none";
   }
   
+  async function getData(fromUrl) {
+    try {
+      const response = await fetch(fromUrl);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
