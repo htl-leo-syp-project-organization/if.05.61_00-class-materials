cb70f782651ad23b34b15cdcafa22f6d78797726
Peter Bauer <peter.bauer@bajupa.com>
2025-03-04 19:06:37 +0100

    Separated loading from rendering

--- Units/REST/public/js/main.mjs
+++ Units/REST/public/js/main.mjs
@@ -1,24 +1,27 @@
 import { getData } from './rest.mjs'
 import { renderOneGridLine } from './html-renderer.mjs'
 
-document.body.onload = function() {
+document.body.onload = async function() {
     const burgerMenuButton = document.querySelector('#burger-menu-button')
     burgerMenuButton.addEventListener('click', showSidebar)
 
     const closeSidebarItems = document.querySelector('#my-sidebar').childNodes
     closeSidebarItems.forEach(item => item.addEventListener('click', hideSidebar))
-    loadDataFromServer()
+    const entries = await loadDataFromServer()
+    renderBlogEntries(entries)
 }
 
 async function loadDataFromServer() {
     const blogEntries = await getData("http://localhost:3000/posts")
+    return blogEntries
+}
+
+function renderBlogEntries(blogEntries) {
     const grid = document.querySelector('#food-grid')
     for (let i = 0; i < blogEntries.length; i+=4) {
         const oneGridLine = renderOneGridLine(blogEntries.slice(i, i+4))
         grid.appendChild(oneGridLine)
     }
-    // renderOneGridLine(blogEntries.slice(0, 4))
-    console.log(blogEntries)
 }
 
     // Script to open and close sidebar
