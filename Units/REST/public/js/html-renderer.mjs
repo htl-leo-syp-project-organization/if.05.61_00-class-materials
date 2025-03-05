9bde928c7ea1c04431813eec3c11ce9b8dfde21b
Peter Bauer <peter.bauer@bajupa.com>
2025-03-05 20:38:44 +0100

    Cramped id into container div

--- Units/REST/public/js/html-renderer.mjs
+++ Units/REST/public/js/html-renderer.mjs
@@ -19,38 +19,63 @@ function renderOneGridLine(blogEntries) {
 }
   
 function renderOneGridCell(blogEntry) {
-  const newDiv = document.createElement('div')
+  const newDiv=createContainerDiv(blogEntry.id)
+  const newImg=createBlogImage(blogEntry)
+  const likeSymbol=createLikeSymbol()
+  const newH3=createBlogTitle(blogEntry)
+  const newP=createBlogText(blogEntry)
+  putGridCellTogether(newDiv, newImg, likeSymbol, newH3, newP)
+  return newDiv
+}
+
+function createContainerDiv(id) {
+  const newDiv=document.createElement('div')
+  newDiv.id=id // id is used to identify the blog entry; bad bad bad; don't try this at home
   newDiv.classList.add('w3-quarter')
+  return newDiv
+}
 
-  const newImg = document.createElement('img')
-  newImg.src = blogEntry.imagePath
-  newImg.alt = 'food image'
-  newImg.style.width = '100%'
+function createBlogImage(blogEntry) {
+  const newImg=document.createElement('img')
+  newImg.src=blogEntry.imagePath
+  newImg.alt='food image'
+  newImg.style.width='100%'
+  return newImg
+}
 
-  const likeSymbol = document.createElement('img')
-  likeSymbol.src = 'images/heart-empty.png'
-  likeSymbol.alt = 'like symbol'
-  likeSymbol.style.width = '20px'
-  likeSymbol.style.height = '20px'
-  likeSymbol.style.position = 'relative'
-  likeSymbol.style.left = '40%'
-  likeSymbol.style.top = '0px'
-  likeSymbol.style.zIndex = '1'
-  likeSymbol.style.cursor = 'pointer'
+function createLikeSymbol() {
+  const likeSymbol=document.createElement('img')
+  likeSymbol.src='images/heart-empty.png'
+  likeSymbol.alt='like symbol'
+  likeSymbol.style.width='20px'
+  likeSymbol.style.height='20px'
+  likeSymbol.style.position='relative'
+  likeSymbol.style.left='46.5%'
+  likeSymbol.style.top='0px'
+  likeSymbol.style.zIndex='1'
+  likeSymbol.style.cursor='pointer'
   likeSymbol.addEventListener('click', () => {
-    likeSymbol.src = 'images/heart-red.png'
+    likeSymbol.src='images/heart-red.png'
   })
+  return likeSymbol
+}
 
-  const newH3 = document.createElement('h3')
-  newH3.textContent = blogEntry.title
+function createBlogTitle(blogEntry) {
+  const newH3=document.createElement('h3')
+  newH3.textContent=blogEntry.title
+  return newH3
+}
 
-  const newP = document.createElement('p')
-  newP.textContent = blogEntry.text
+function createBlogText(blogEntry) {
+  const newP=document.createElement('p')
+  newP.textContent=blogEntry.text
+  return newP
+}
 
+function putGridCellTogether(newDiv, newImg, likeSymbol, newH3, newP) {
   newDiv.appendChild(newImg)
   newDiv.appendChild(likeSymbol)
   newDiv.appendChild(newH3)
   newDiv.appendChild(newP)
-
-  return newDiv
 }
+
