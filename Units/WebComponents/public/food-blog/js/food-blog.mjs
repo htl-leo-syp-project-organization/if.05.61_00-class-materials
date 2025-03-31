c70c8e28be174d528bcfe8b3384715db032ab1ec
Peter Bauer <peter.bauer@bajupa.com>
2025-03-31 23:41:51 +0200

    Fixed update button

--- Units/WebComponents/public/food-blog/js/food-blog.mjs
+++ Units/WebComponents/public/food-blog/js/food-blog.mjs
@@ -12,7 +12,6 @@ window.onload = function() {
 
     const updateButton = document.querySelector('#update-button')
     updateButton.addEventListener('click', () => {
-        const foodBlogEntry = document.querySelector('food-blog-entry')
-        foodBlogEntry.data.liked = true
+        foodBlogEntry.toggleLike()
     })
 }
